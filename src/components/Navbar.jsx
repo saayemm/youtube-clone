a3ca9040ab1028c7729050cc45bdfdece711e0fe
Saayemm <sayemdcc@gmail.com>
2025-02-18 17:50:55 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <div>logo</div>
+      <div>logo</div>
+      <div>logo</div>
+    </div>
+  )
+}
+
+export default Navbar
